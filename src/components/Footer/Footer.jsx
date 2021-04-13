94e8bdfe9066c0b0b2bf86dd336af0f5de88bf40
Slawomir Pietrzak <1pietrzakslawomir@gmail.com>
2021-04-13 15:34:17 +0200

    Get apply footer section.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 
 import bemCssModules from 'bem-css-modules';
 import { default as FooterStyles } from './Footer.module.scss';
@@ -7,7 +10,40 @@ const block = bemCssModules(FooterStyles);
 const Footer = () => {
   return (
     <footer className={block()}>
-      <p>Footer</p>
+
+      <section className={block('social')}>
+        <p className={block('social__title')}>Znajdź nas na: </p>
+        <a className={block('a')}
+          href="https://www.facebook.com/krysztal.szklo">
+          <FaFacebookSquare className={block('icon')} />
+        </a>
+        <a className={block('a')}
+          href="https://www.youtube.com/channel/UC4vIjhmPZlTB89xZx08A_3w">
+          <FaYoutube className={block('icon')} />
+        </a>
+        <a className={block('a')}
+          href="https://allegro.pl/uzytkownik/Krysztal_Szklo?bmatch=cl-e2101-d3681-c3682-uni-1-4-0319">
+          <FaShoppingCart className={block('icon')} />
+        </a>
+      </section>
+
+      <section className={block('extra_info')}>
+        <a className={block('a')} href=".">
+          <p className={block('p')}>Kontakt</p>
+        </a>
+
+        <a className={block('a')} href=".">
+          <p className={block('p')}>Polityka prywatności i sprzedaży</p>
+        </a>
+
+        <a className={block('a')} href=".">
+          <p className={block('p')}>Katalogi</p>
+        </a>
+
+      </section>
+      <section className={block('create_by')}>
+        <p className={block('p')}>Page was created by: <a className={block('a')} href="https://www.linkedin.com/in/slawomir-pietrzak/">Treodaio</a></p>
+      </section>
     </footer>
   );
 }
